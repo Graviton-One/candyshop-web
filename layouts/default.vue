c6d24b6a94c0761377c3f8b27aba5b4528f21d4a
MerkulovKA <kirill_tagil9@mail.ru>
2021-10-05 19:58:48 +0500

    shop page

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,11 @@
     <mobile-menu />
 
     <main class="flex-grow relative z-[1]">
-      <div class="w-full h-full z-[-1] absolute left-0 top-0">
-        <div class="container h-full pb-40">
-          <img src="~/assets/img/spiral.svg" class="w-full h-full object-center object-contain" alt="">
-        </div>
-      </div>
+<!--      <div class="w-full h-full z-[-1] absolute left-0 top-0">-->
+<!--        <div class="container h-full pb-40">-->
+<!--          <img src="~/assets/img/spiral.svg" class="w-full h-full object-center object-contain" alt="">-->
+<!--        </div>-->
+<!--      </div>-->
 
       <nuxt />
     </main>
